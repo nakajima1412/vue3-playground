0463a70c2bfeb86f505d0461ffab89c768f6d482
nakajima takayuki <e09228@gmail.com>
2022-11-30 02:03:53 +0900

    add: tiptap

---
+++ src/components/TiptapEditor.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Mention from '@tiptap/extension-mention';
+import StarterKit from '@tiptap/starter-kit';
+import { useEditor, EditorContent } from '@tiptap/vue-3';
+
+const editor = useEditor({
+  content: '',
+  editorProps: {
+    attributes: {
+      class:
+        'prose prose-sm sm:prose lg:prose-lg xl:prose-2xl mx-auto focus:outline-none',
+    },
+  },
+  extensions: [
+    StarterKit,
+    // Mention.configure({
+    //   HTMLAttributes: {
+    //     class: 'mention',
+    //   },
+    // }),
+  ],
+});
+</script>
+
+<template>
+  <EditorContent :editor="editor"></EditorContent>
+</template>
---
+++ src/views/EditorView.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <TiptapEditor
+      style="border: 2px solid #ebebeb; border-radius: 8px; min-height: 200px"
+    />
+  </div>
+</template>
+
+<script setup lang="ts">
+import TiptapEditor from '@/components/TiptapEditor.vue';
+</script>
